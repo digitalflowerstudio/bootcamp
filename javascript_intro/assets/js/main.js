9260e103297ab37e0bcc6aebce069ddb4ba4d4e1
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-06 12:45:47 +0100

    started working on tasks for javascript intro

--- javascript_intro/assets/js/main.js
+++ javascript_intro/assets/js/main.js
@@ -69,6 +69,111 @@ console.log("10" + "4"); // Führt eine Zeichenketten-Kombination aus (Beim Plus
 console.log("4" * "2");
 console.log("4" - '4');
 
+// NaN steht für Not a Number
+
 console.log(4 + 20);
+
+// ? Boolean
+// Werden auch LILA ausgegeben und sie haben nur zwei mögliche werte: true oder false
+console.log(true);
+console.log(false);
+
+// Konventionen
+// ? Selbsterklärend, nicht zu kurz / nicht zu lang / dürfen nicht mit zahlen anfang
+// z.B 2Pac besser als twoPac - immer auch camelCase
+
+// ! CASE SENSITIVE
+
+// Underscore
+
+let my_first_variable;
+
+// PascalCase
+
+let MyFirstVariable;
+
+// CamelCase
+
+let myFirstVariable;
+
+// Was nicht geht:
+// ! let nein-nein-so-nicht
+// ! let kannichtsonichtlesen
+// ! let $bittesoauchnicht;
+
+// String Concatination
+console.log('String Concatination');
+
+const firstName = "Wade";
+const secondName = "Winston";
+const lastName = "Wilson";
+
+let state = "New York";
+let leer = ' ';
+
+console.log('Ich bin' + leer + firstName + leer + secondName + leer + lastName + leer + 'und lebe in' + leer + state);
+console.log(`Ich bin ${firstName} ${secondName} ${lastName} und ich lebe in ${state}`);
+
+// Arithmetische Operatoren
+
+let x = 5;
+let y = 3;
+let z = 15;
+
+//addieren
+console.log(x + y);
+//subtrahieren
+console.log(x - y);
+//dividieren
+console.log(x / y);
+//multiplizieren
+console.log(x * y);
+
+// Modulo % = Restwert (teilen und dann restwert)
+console.log(15 % 5);
+console.log(5 % 2);
+console.log(15 % 4);
+
+// increment - mit ++ können wird der wert um eins erhöht / ein + gerechnet
+
+let zahl1 = 2
+console.log({zahl1});
+zahl1++;
+zahl1++;
+console.log({zahl1});
+
+// decrement -- wird eins vom vorherigen wert abgezogen 
+
+let zahl2 = 5;
+console.log({zahl2});
+zahl2--; //wird um 1 reduziert
+zahl2--; //wird um 1 reduziert
+console.log({zahl2}); // gibt uns 3
+
+
+// Output
+//documentwrite
+
+document.write("hallo leute")
+document.write("<h2>Ich bin eine Headline</h2>")
+
+// window.alert("WARNING!!!")
+
+// Input
+// window.prompt("Wie alt bist du?")
+
+// window.confirm("Stimme zu")
+
+let confirm = window.confirm("Stimmen Sie den Cookies zu?")
+console.log(confirm);
+
+
+
+
+
+
+
+
+
 
 
