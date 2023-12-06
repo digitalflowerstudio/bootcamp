5495ac57d65486cf53df3ce1d4ccb46984f8a917
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-06 14:14:34 +0100

    dom1_1 fertig

---
+++ javascript_intro/aufgaben/DOM/DOMlevel1_1/assets/js/main.js
@@ -0,0 +1,10 @@
+document.write('<h1>Hello World!</h1>')
+document.write('<p>Have a nice day!</p>')
+
+let myText = 'Hello again'
+document.write(myText)
+
+let vorName = 'Fritz'
+let nachName = 'Müller'
+
+document.write(`<h2>Hi ich bin ${vorName} ${nachName}</h2>`)
