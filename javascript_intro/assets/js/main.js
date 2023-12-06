20c1efc1daafdd1ff5c5632a0a269dc2eb6cf1a3
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-06 09:39:30 +0100

    js intro

---
+++ javascript_intro/assets/js/main.js
@@ -0,0 +1,33 @@
+// Console.log()
+
+console.log("Hello World");
+// mit console.log() können wir uns Texte (strings) ausgeben lassen.
+
+// Variable
+
+// - const - Werte können nicht mehr geändert werden
+// - let - Werte können noch einmal verändert werden
+// var (obsolet)
+
+// ? CONST
+
+const trainerName = "Farid";
+
+console.log(trainerName);
+
+// ? LET
+
+let score = 8;
+console.log(score);
+
+let num1 = 5;
+console.log("Das ist Num1:", num1);
+
+let num2 = 55;
+console.log("Das ist Num2:", num2);
+
+let num3 = 44;
+console.log({num3});
+
+num1 = 105;
+console.log({num1});
