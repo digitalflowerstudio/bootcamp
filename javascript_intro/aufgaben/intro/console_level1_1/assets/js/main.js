9654b060ceba47c762c0d5d3fac9f3e524be9e90
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-06 13:59:07 +0100

    console 1_2 fertig

---
+++ javascript_intro/aufgaben/intro/console_level1_1/assets/js/main.js
@@ -0,0 +1,24 @@
+// 1. Beispiel
+
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+let helloWorld = 'Hello World';
+console.log(helloWorld);
+
+let leer = ' ';
+
+const firstName = 'Anton';
+let age = 28;
+console.log(firstName + leer + age);
+
+let job = 'Trainer:in'
+console.log(job);
+
+let married = true;
+console.log(married);
+
+console.log(`${firstName} ist ${age} Jahre alt, von Berufe ${job} und ist verheiratet: ${married}`);
+
+document.getElementById('output').innerHTML = b;
