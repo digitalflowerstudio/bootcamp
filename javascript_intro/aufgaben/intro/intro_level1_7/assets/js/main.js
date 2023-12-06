34e922c6d4816637968db338f459ed1aa4ef1583
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-06 13:01:54 +0100

    intro 1_7 fertig

---
+++ javascript_intro/aufgaben/intro/intro_level1_7/assets/js/main.js
@@ -0,0 +1,14 @@
+let helloWorld = 'Hello World';
+console.log(helloWorld);
+
+const pi = 3.14;
+console.log(pi);
+
+const burjDubai = "829m";
+console.log(burjDubai);
+
+let camelCase = "camelCase";
+console.log(camelCase);
+
+let kurzStatus = true;
+console.log(kurzStatus);
