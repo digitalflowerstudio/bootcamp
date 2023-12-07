fe49d0050d344a0cd34c1b76b8943d826b0795c3
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-07 13:41:38 +0100

    level1_2fertig

---
+++ javascript_tag2/functions/functions_level1_2/assets/js/main.js
@@ -0,0 +1,7 @@
+function intro2(paramName){
+    const studentName = 'SuperCoder:in';
+    console.log(`Hi ${studentName}. Mein Name ist ${paramName}.`);
+}
+
+intro2('Max Mustermann')
+intro2('Frank the Tank')
