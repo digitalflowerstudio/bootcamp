cd94cc126b965db1aae1cabc5479fe2b385cc7fe
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-07 14:35:25 +0100

    dom level 1_3 fertig

---
+++ javascript_tag2/dom2/dom_level_1_3/assets/js/main.js
@@ -0,0 +1,8 @@
+const gallery = document.getElementById('gallery')
+console.log(gallery);
+
+gallery.innerHTML += ('<figure><img src="https://source.unsplash.com/random/300×300"</img><figcaption>Fig1</figcaption></figcaption></figure>')
+
+gallery.innerHTML += ('<figure><img src="https://source.unsplash.com/random/300×300"</img><figcaption>Fig2</figcaption></figcaption></figure>')
+
+gallery.innerHTML += ('<figure><img src="https://source.unsplash.com/random/300×300"</img><figcaption>Fig3</figcaption></figcaption></figure>')
