88ea9ca88cd2ecf8c134c2920eee5f0a63a348c0
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-07 15:11:28 +0100

    dom 1_4 fertig

---
+++ javascript_tag2/dom2/dom_level_1_4/assets/js/main.js
@@ -0,0 +1,28 @@
+document.getElementById("info").innerHTML =
+  "<h1>Hello World</h1>" + "<h2>How are you?</h2>";
+
+document.getElementById("container").innerHTML = "<p>start of the element</p>";
+
+function magic() {
+  let outPut = document.getElementById("myText").value;
+  document.getElementById("wurst").innerHTML = outPut;
+}
+
+function moreMagic() {
+  let datum = (document.getElementById("wurst").innerHTML +=
+    "<h1>WURSTSALAT IST EINE H1</h1>" + '<br>');
+}
+
+function thePowerOfNow() {
+  const now = new Date();
+  const currentDateTime = now.toLocaleString();
+  document.getElementById(
+    "wurst"
+  ).innerHTML += `<br>zeit und tag ist ${currentDateTime}`;
+}
+
+function funcyTime() {
+  magic();
+  thePowerOfNow();
+  moreMagic();
+}
