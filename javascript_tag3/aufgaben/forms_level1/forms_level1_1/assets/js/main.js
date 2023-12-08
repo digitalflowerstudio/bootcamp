815605f9bed8f6e3c0e626b9ef327c90bf9f8059
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-08 13:17:52 +0100

    levecomparison_fertig

---
+++ javascript_tag3/aufgaben/forms_level1/forms_level1_1/assets/js/main.js
@@ -0,0 +1,12 @@
+const num = 5;
+const anotherNum = 8;
+
+console.log(num == anotherNum); //false
+console.log(num > anotherNum); //false
+console.log(num < anotherNum); //true
+console.log(num >= anotherNum); //false
+console.log(num <= anotherNum); //true
+
+console.log(num == "5"); //true
+console.log(num != 8); //true
+console.log(num != "5"); //false
