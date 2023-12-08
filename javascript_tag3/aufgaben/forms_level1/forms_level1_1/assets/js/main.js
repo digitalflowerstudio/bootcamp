f58d3f167053bda9397ebe46f717a921664d9676
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-08 13:36:22 +0100

    forms level1_1 fertig

--- javascript_tag3/aufgaben/forms_level1/forms_level1_1/assets/js/main.js
+++ javascript_tag3/aufgaben/forms_level1/forms_level1_1/assets/js/main.js
@@ -1,12 +1,5 @@
-const num = 5;
-const anotherNum = 8;
-
-console.log(num == anotherNum); //false
-console.log(num > anotherNum); //false
-console.log(num < anotherNum); //true
-console.log(num >= anotherNum); //false
-console.log(num <= anotherNum); //true
-
-console.log(num == "5"); //true
-console.log(num != 8); //true
-console.log(num != "5"); //false
+function getInputValue(){
+    const output = document.querySelector(".output");
+    let numba = document.querySelector(".numba").value;
+    output.innerHTML += `Your numba doubled is ${numba * 2}`
+}
