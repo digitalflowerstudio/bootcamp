186e74545388e164b0a8edfd505a45ca2caf78dd
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-08 14:49:18 +0100

    classlist 2_1 done

---
+++ javascript_tag3/aufgaben/classlist/classlist2_1/assets/js/main.js
@@ -0,0 +1,9 @@
+function changeHeadline(){
+    const headLine = document.querySelector("#epicHeadline")
+    headLine.classList.add("headLine")
+}
+
+function resetHeadline(){
+    const headLine = document.querySelector("#epicHeadline")
+    headLine.classList.remove("headLine")
+}
