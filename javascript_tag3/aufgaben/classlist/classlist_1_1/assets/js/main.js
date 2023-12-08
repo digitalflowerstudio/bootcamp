99283cbc95dd737c4cfbff9d3f3c428afaa97269
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-08 14:37:00 +0100

    classlist level1_1 done

---
+++ javascript_tag3/aufgaben/classlist/classlist_1_1/assets/js/main.js
@@ -0,0 +1,9 @@
+function headlineChange(){
+    const headLine = document.querySelector("#bucketListHeading");
+    headLine.classList.toggle("bucketHead");
+}
+
+function listChange(){
+    const list = document.querySelector("#bucketList");
+    list.classList.toggle("bucketList");
+}
