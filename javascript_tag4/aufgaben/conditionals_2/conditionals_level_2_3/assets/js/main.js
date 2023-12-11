882a2a4b28e805d45007f5eadd40b68747f4f149
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-11 15:53:17 +0100

    2_3 fertig

---
+++ javascript_tag4/aufgaben/conditionals_2/conditionals_level_2_3/assets/js/main.js
@@ -0,0 +1,15 @@
+function calcDifference() {
+  const zahlOne = 27;
+  let zahlVar = 300;
+
+  let difference = zahlVar - zahlOne;
+
+  if (difference >= 27) {
+    difference = difference * 2;
+    console.log(difference);
+  } else {
+    console.log(difference);
+  }
+}
+
+calcDifference();
