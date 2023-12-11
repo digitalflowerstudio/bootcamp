440b9b6104d35cd35fb4d7804eff6bf674458549
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-11 10:13:05 +0100

    js practice

--- javascript_tag4/assets/js/main.js
+++ javascript_tag4/assets/js/main.js
@@ -1,1 +1,17 @@
+const output = document.querySelector(".output");
+let outputValue = 0;
 
+function superFunc(value){
+    outputValue = outputValue + value;
+    output.innerHTML = outputValue;
+}
+
+function superFuncZwei(){
+    outputValue = outputValue * 2;
+    output.innerHTML = outputValue;
+}
+
+function reset(){
+    outputValue = 0;
+    output.innerHTML = outputValue;
+}
