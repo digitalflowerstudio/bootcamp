a93456994f389e09cc3f9818905a142d6bb0ff9a
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-11 15:19:43 +0100

    1_3 fertig

--- javascript_tag4/aufgaben/conditionals_1/conditionals_level_1_3/assets/js/main.js
+++ javascript_tag4/aufgaben/conditionals_1/conditionals_level_1_3/assets/js/main.js
@@ -31,21 +31,19 @@
 //   }
 // }
 
-const formular = document.querySelector("form");
-
 function greaterThan() {
   event.preventDefault();
 
-  let decision = "";
+  let result = "";
   let output = document.querySelector(".output");
 
   if (document.getElementById("input").value >= 18) {
-    decision = true;
-    console.log(decision);
+    result = true;
+    console.log(result);
     output.textContent = "alt genug";
   } else {
-    decision = false;
-    console.log(decision);
+    result = false;
+    console.log(result);
     output.textContent = "zu jung";
   }
 }
