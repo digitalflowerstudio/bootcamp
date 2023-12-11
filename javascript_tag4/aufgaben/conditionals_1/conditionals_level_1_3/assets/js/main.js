d33a489b4fcc08cc06b71d911d5bb5746afe76f8
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-11 15:18:25 +0100

    update level 1_3

--- javascript_tag4/aufgaben/conditionals_1/conditionals_level_1_3/assets/js/main.js
+++ javascript_tag4/aufgaben/conditionals_1/conditionals_level_1_3/assets/js/main.js
@@ -9,13 +9,43 @@
 //   }
 // }
 
-function greaterThan() {
-  let age = document.getElementById("input").value;
-  let resultNew = document.querySelector(".outputNew");
+// function greaterThan() {
+//   event.preventDefault();
+//   let outputNew = document.querySelector(".outputNew");
 
-  if (age >= 18) {
-    resultNew.textContent = "Ja, du kannst Shisha rauchen!";
+//   let result = "";
+
+//   if (document.getElementById("input").value >= 18) {
+//     result = true;
+//   } else {
+//     result = false;
+//   }
+
+//   switch (result) {
+//     case true:
+//       console.log("alt genug");
+//       outputNew.innerHTML = "Du bist alt genug";
+//     case false:
+//       console.log("zu jung");
+//       outputNew.innerHTML = "Du bist zu jung";
+//   }
+// }
+
+const formular = document.querySelector("form");
+
+function greaterThan() {
+  event.preventDefault();
+
+  let decision = "";
+  let output = document.querySelector(".output");
+
+  if (document.getElementById("input").value >= 18) {
+    decision = true;
+    console.log(decision);
+    output.textContent = "alt genug";
   } else {
-    resultNew.textContent = "Du darfst noch nicht Shisha rauchen!";
+    decision = false;
+    console.log(decision);
+    output.textContent = "zu jung";
   }
 }
