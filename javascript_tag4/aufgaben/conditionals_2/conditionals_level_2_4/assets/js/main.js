4d23623fe641a72a827e46ddaa6566a700c95516
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-11 15:58:12 +0100

    2_4 fertig

---
+++ javascript_tag4/aufgaben/conditionals_2/conditionals_level_2_4/assets/js/main.js
@@ -0,0 +1,15 @@
+let a = 130;
+let b = 130;
+
+function sumOf(nummer1, nummer2) {
+  let summe = nummer1 + nummer2;
+  return summe;
+}
+
+let c = sumOf(a, b);
+
+if (a == b) {
+  console.log(c * 5);
+} else {
+  console.log(c);
+}
