c47cb9b035903bce5d8ce1a53b2ffa9e28ce43d5
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-12 12:32:24 +0100

    level1_1 done

--- javascript_tag5/tasks/string_methods01/string_methods_level1_1/assets/js/main.js
+++ javascript_tag5/tasks/string_methods01/string_methods_level1_1/assets/js/main.js
@@ -1,11 +1,16 @@
-let firstName = "Florian";
-let lastName = "Mueller";
-let fullName = firstName + lastName;
+let firstName = "M.";
+let midName = "Night";
+let lastName = "Shyamalan";
+let space = " ";
+let fullName = firstName + space + midName + space + lastName;
 
 let lenFirst = firstName.length;
 let lenLast = lastName.length;
+let lenMid = midName.length;
 let lenFull = fullName.length;
 
 console.log(lenFirst);
 console.log(lenLast);
 console.log(lenFull);
+
+console.log(fullName);
