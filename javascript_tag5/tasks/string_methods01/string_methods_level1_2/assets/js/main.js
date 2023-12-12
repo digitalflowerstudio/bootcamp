1913a25fb2ceabb8d191f2d7e44b15e99c2e1c7a
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-12 12:44:05 +0100

    level1_2done

---
+++ javascript_tag5/tasks/string_methods01/string_methods_level1_2/assets/js/main.js
@@ -0,0 +1,9 @@
+const text = "How aliens on Earth, are like humans on Mars";
+
+let indexOne = text.indexOf("h");
+let indexTwo = text.indexOf("Earth");
+let indexThree = text.indexOf("Moon");
+
+console.log(indexOne, indexTwo, indexThree);
+
+// Moon is not in the String and therefor it's -1
