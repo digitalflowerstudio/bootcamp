2af8bd4347cd12f2c3fd2c9e8891157bddea01ea
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-12 14:32:05 +0100

    level2_3done

--- javascript_tag5/tasks/string_methods02/string_methods_level2_3/assets/js/main.js
+++ javascript_tag5/tasks/string_methods02/string_methods_level2_3/assets/js/main.js
@@ -2,7 +2,7 @@ function highlightText() {
   let searchTerm = document.getElementById("search-input").value;
   let articleContent = document.getElementById("article").innerHTML;
 
-  // RegExp ist eine regular expression. Man kann ihr flas geben. gi steht für global und i - das heisst es such nach allen instanzen des suchbegriffs und i bedeutet, dass es case INsensitiv ist, das heisst es ist egal ob groß oder klein
+  // RegExp ist eine regular expression. Man kann ihr flags geben. gi steht für global - das heisst es such nach allen instanzen des suchbegriffs und i bedeutet, dass es case INsensitiv ist, das heisst es ist egal ob groß oder klein
   let regex = new RegExp(searchTerm, "gi");
   // jede instanz des suchbegriffes wird als argument match gespeichert und dann via return in einen span gepackt mit dem style highlight aus der css,
   let highlightedContent = articleContent.replace(regex, function (match) {
