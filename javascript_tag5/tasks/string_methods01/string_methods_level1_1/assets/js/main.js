c755e0c01ce51ae8bcbe1fbd4800d9d2e51b8d11
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-12 12:29:44 +0100

    string_methods_level_1_1_done

---
+++ javascript_tag5/tasks/string_methods01/string_methods_level1_1/assets/js/main.js
@@ -0,0 +1,11 @@
+let firstName = "Florian";
+let lastName = "Mueller";
+let fullName = firstName + lastName;
+
+let lenFirst = firstName.length;
+let lenLast = lastName.length;
+let lenFull = fullName.length;
+
+console.log(lenFirst);
+console.log(lenLast);
+console.log(lenFull);
