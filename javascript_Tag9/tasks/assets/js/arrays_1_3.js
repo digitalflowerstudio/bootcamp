29b490dd6a42630fa3f1a4e9eecdb99808128a1b
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-18 13:07:49 +0100

    Array Level 1_3 completed

---
+++ javascript_Tag9/tasks/assets/js/arrays_1_3.js
@@ -0,0 +1,29 @@
+// First Array
+
+let firstName = "John";
+let lastName = "Walker";
+let job = "Janitor";
+
+let person = [firstName, lastName, job];
+
+console.log(person.length);
+
+// Second Array
+
+let firstFriend = "Jim";
+let secondFriend = "Lucas";
+let thirdFriend = "Keanu";
+
+let friends = [firstFriend, secondFriend, thirdFriend];
+
+console.log(friends.length);
+
+// Third Array
+
+let food1 = "Meat";
+let food2 = "Veggies";
+let food3 = "Rice";
+
+let favFood = [food1, food2, food3];
+
+console.log(favFood.length);
