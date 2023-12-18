fc594f3b15773abb6320fe2da1e90cac06c18e52
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-18 13:31:08 +0100

    level 1_7 and level 1_9 done

---
+++ javascript_Tag9/tasks/assets/js/arrays_1_9.js
@@ -0,0 +1,16 @@
+let values = [23, 57, 75];
+console.log(values);
+
+values.push(1, 2, 3, 4, 5);
+console.log(values);
+
+values.unshift(1, 2, 3, 4, 5);
+console.log(values);
+
+values.pop();
+values.pop();
+console.log(values);
+
+values.shift();
+values.shift();
+console.log(values);
