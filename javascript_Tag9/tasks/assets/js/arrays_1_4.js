774f942055c880bd27e90e53789cd9f2e93fe431
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-18 13:12:29 +0100

    Arrays 1_4 completed

---
+++ javascript_Tag9/tasks/assets/js/arrays_1_4.js
@@ -0,0 +1,47 @@
+// First Array
+
+let firstName = "John";
+let lastName = "Walker";
+let job = "Janitor";
+
+let person = [firstName, lastName, job];
+
+console.log(person);
+
+let home = "Earth";
+
+person.push(home);
+
+console.log(person);
+
+// Second Array
+
+let firstFriend = "Jim";
+let secondFriend = "Lucas";
+let thirdFriend = "Keanu";
+
+let friends = [firstFriend, secondFriend, thirdFriend];
+
+console.log(friends);
+
+let fourthFriend = "Bob";
+
+friends.push(fourthFriend);
+
+console.log(friends);
+
+// Third Array
+
+let food1 = "Meat";
+let food2 = "Veggies";
+let food3 = "Rice";
+
+let favFood = [food1, food2, food3];
+
+console.log(favFood);
+
+let food4 = "Pizza";
+
+favFood.push(food4);
+
+console.log(favFood);
