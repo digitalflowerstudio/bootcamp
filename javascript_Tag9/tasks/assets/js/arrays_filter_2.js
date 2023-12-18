c19cad4f7f858708762ce8e99d737935ebea5457
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-18 15:25:41 +0100

    filter 2 update

--- javascript_Tag9/tasks/assets/js/arrays_filter_2.js
+++ javascript_Tag9/tasks/assets/js/arrays_filter_2.js
@@ -10,6 +10,24 @@ const words = [
   "Krokodil",
   "Lampe",
 ];
-
+// sobald ich mehrzeilige funktionen mit { } habe brauche ich return
 const result = words.filter((word) => word.length > 6);
 console.log(result);
+
+const arrowFunc = () => {
+  const result = words.filter((word) => {
+    return word.length > 6;
+  });
+  return result;
+};
+
+console.log(arrowFunc());
+
+function normalFunc() {
+  const result = words.filter((word) => {
+    return word.length > 6;
+  });
+  return result;
+}
+
+console.log(normalFunc());
