50c7714bc51f1943fe66fedae95d837e8cc970b9
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-19 14:58:33 +0100

    slice 1_8 done

---
+++ javascript_tag10/tasks/slice_split/array_level_1_8/assets/js/main.js
@@ -0,0 +1,12 @@
+const lieblingsreiseziele = [
+  "Bahamas",
+  "Cayman Islands",
+  "Panama",
+  "Isle of Man",
+  "Switzerland",
+  "Malta",
+];
+
+console.log(lieblingsreiseziele);
+
+console.log(lieblingsreiseziele.slice(0, 4));
