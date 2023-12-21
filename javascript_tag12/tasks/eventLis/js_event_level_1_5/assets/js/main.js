20ee108fc505d75f5b18f7090e62cc7e64504826
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-21 15:19:56 +0100

    removed text variable

--- javascript_tag12/tasks/eventLis/js_event_level_1_5/assets/js/main.js
+++ javascript_tag12/tasks/eventLis/js_event_level_1_5/assets/js/main.js
@@ -1,8 +1,7 @@
-let text = document.getElementById("clickMe").innerHTML;
 let count = 0;
 let button = document.getElementById("clickMe");
 
 button.addEventListener("click", function () {
   count += 1;
-  button.innerHTML = text.replace(text, `Click me ${count}`);
+  button.innerHTML = `Click me ${count}`;
 });
