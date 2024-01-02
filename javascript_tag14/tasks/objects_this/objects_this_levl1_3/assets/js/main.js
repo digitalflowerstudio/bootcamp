7bcbc8b623d739ae4dd26252fe76e19e02f803b6
digitalflowerstudio <floriantappeser@gmail.com>
2024-01-02 15:50:27 +0100

    this level 1_3 done

---
+++ javascript_tag14/tasks/objects_this/objects_this_levl1_3/assets/js/main.js
@@ -0,0 +1,14 @@
+let personFunction = {
+  fullName: function () {
+    return this.vorname + " " + this.nachname;
+  },
+};
+let personObjekt = {
+  vorname: "Anton",
+  nachname: "Fish",
+};
+
+let anzeigeImHTMLDokument = `<p>${personFunction.fullName.call(
+  personObjekt
+)}</p>`;
+document.getElementById("person").innerHTML = anzeigeImHTMLDokument;
